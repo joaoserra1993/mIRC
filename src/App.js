471f18655f257b717e796de21abeece1061e9a26
João Serra [1221842] <1221842@isep.ipp.pt>
2023-07-21 12:09:47 +0100

    Finished the first version of the app, CSS almost inexistent

--- src/App.js
+++ src/App.js
@@ -3,37 +3,48 @@ import {Auth} from "./components/auth/Auth";
 import {useRef, useState} from "react";
 import Cookies from "universal-cookie";
 import {Chat} from "./components/chat/Chat";
+import {signOut} from "firebase/auth";
+import {auth} from './firebase-config';
 
 const cookies = new Cookies();
 
 function App() {
-    const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
-    const [room, setRoom] = useState(null);
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
 
-    const roomInputRef = useRef(null);
+  const roomInputRef = useRef(null);
+  const signOutUser = async () => {
+    await signOut(auth)
+    cookies.remove("auth-token")
+    setIsAuth(false)
+    setRoom(null)
+  };
 
-    if (!isAuth) {
-        return (
-            <div>
-                <Auth setIsAuth={setIsAuth}/>
-            </div>
-        );
-    }
+  if (!isAuth) {
     return (
-        <div>
-            {room ? (
-                <Chat room={room}/>
-            ) : (
-                <div className="room">
-                    <label> Enter room name:</label>
-                    <input ref={roomInputRef}/>
-                    <button onClick={() => setRoom(roomInputRef.current.value)}>
-                        Enter chat
-                    </button>
-                </div>
-            )}
-        </div>
+      <div>
+        <Auth setIsAuth={setIsAuth}/>
+      </div>
     );
+  }
+  return (
+    <>
+      {room ? (
+        <Chat room={room}/>
+      ) : (
+        <div className="room">
+          <label> Enter room name:</label>
+          <input ref={roomInputRef}/>
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter chat
+          </button>
+        </div>
+      )}
+    <div className="sign-out">
+      <button onClick={signOutUser}>Sign Out</button>
+    </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,13 +1,30 @@
-import {useState} from "react";
-import {addDoc, collection, serverTimestamp} from 'firebase/firestore';
+import {useEffect, useState} from "react";
+import {addDoc, collection, onSnapshot, query, serverTimestamp, where, orderBy} from 'firebase/firestore';
 import {auth, database} from "../../firebase-config";
+import './Chat.css';
 
 export const Chat = (props) => {
   const {room} = props;
-
   const [newMessage, setNewMessage] = useState("");
-
   const messagesReference = collection(database, "messages");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // this query will retrieve the message sent in a specific room to the user
+    const queryMessages = query(messagesReference,
+      where("room", "==", room),
+      orderBy("createdAt")
+    );
+    const unSubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({...doc.data(), id: doc.id});
+      });
+      setMessages(messages);
+    });
+    return () => unSubscribe();
+  }, []);
+
 
   const handleSubmit = async (event) => {
     // this prevents the page from reloading while submitting the form
@@ -24,6 +41,17 @@ export const Chat = (props) => {
 
   return (
     <div className="chat-app">
+      <div className="header">
+        <h1>{room}</h1>
+      </div>
+      <div className="messages">
+        {messages.map((message) => (
+          <div className="message" key={message.id}>
+            <span className="user">{message.user}</span>
+            {message.text}
+          </div>
+        ))}
+      </div>
       <form onSubmit={handleSubmit} className="new-message-form">
         <input
           className="new-message-input"
