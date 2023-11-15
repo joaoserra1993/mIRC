7d6cefda86140f1c8a7fd4db38906db4309ea78c
joaoserra1993 <107553803+joaoserra1993@users.noreply.github.com>
2023-11-15 15:58:21 +0000

    Added loading, hopefully working!

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,6 +1,8 @@
 import {auth, provider} from '../../firebase-config';
 import {signInWithPopup} from 'firebase/auth';
 import './Auth.css';
+import React, { useState } from 'react';
+
 
 
 // with this, after you signed in, if you refresh the page you'll maintain signed in
@@ -10,19 +12,27 @@ const cookies = new Cookies();
 
 export const Auth = (props) => {
     const {setIsAuth} = props;
+    const [loading, setLoading] = useState(false);
 
 // async waits for an outcome (here the outcome is the sign-in process) and only after that the function will terminate
     const signInWithGoogle = async () => {
+        setLoading(true);
         try {
             const result = await signInWithPopup(auth, provider);
             cookies.set("auth-token", result.user.refreshToken);
             setIsAuth(true);
         } catch (err) {
             console.log(err);
+        } finally {
+            setLoading(false);
         }
-    };
+     };
 
+    if (loading) {
+        return <div class="lds-default"></div>;
+    }
     return (
+        <div className={`auth-container ${loading ? 'loading-page' : ''}`}>
         <div className="auth-container">
             <div className="background"></div>
             <div className="auth">
@@ -30,5 +40,6 @@ export const Auth = (props) => {
                 </button>
             </div>
         </div>
+    </div>
     );
  }
